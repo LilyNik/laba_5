6ac88bac7f199d2ca1bbc51fd8c7accc0a511711
LilyNik <lilyzazylil@gmail.com>
2024-12-23 23:55:53 +0300

    injector.java

--- laba_5/src/main/java/org/example/App.java
+++ laba_5/src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
-package org.example;
+package annotation;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// Указываем, что аннотация будет доступна во время выполнения
+@Retention(RetentionPolicy.RUNTIME)
+// Указываем, что аннотация может применяться только к полям класса
+@Target(ElementType.FIELD)
+public @interface AutoInjectable {
+    // Аннотация без параметров, используемая для маркировки полей,
+    // которые должны быть автоматически инъецированы.
 }
